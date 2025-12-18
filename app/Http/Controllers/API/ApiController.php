ad71018ed65d096c4f644337195a27447fd37ce6
Rubel224603 <cserubelhosen@gmail.com>
2025-12-18 19:29:03 +0600

    login check

--- app/Http/Controllers/API/ApiController.php
+++ app/Http/Controllers/API/ApiController.php
@@ -6,23 +6,25 @@ use App\Http\Controllers\Controller;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
-use function PHPUnit\Runner\validate;
 
-class ApiController extends Controller {
+class ApiController extends Controller
+{
 
-    public function goForLogin(Request $request){
-       try{
-           $request->validate([
-               'email'=>"required|email",
-               'password'=>'required'
-           ]);
-          $user = User::where('email', $request->email)->first();
+    public function goForLogin(Request $request)
+    {
+        try {
+            $request->validate([
+                'email' => "required|email",
+                'password' => 'required'
+            ]);
+
+            $user = User::where('email', $request->email)->first();
 
             if (!$user) {
-              return response()->json([
-                  "success" => false,
-                  "message" => "User not found"
-              ], 404);
+                return response()->json([
+                    "success" => false,
+                    "message" => "User not found"
+                ], 404);
             }
             //if(!($request->password == $user->password)){
             //  return response()->json([
@@ -31,22 +33,22 @@ class ApiController extends Controller {
             //    ],401);
             //
             // }
-           if ($request->password != $user->password) {
-               return response()->json([
-                   "success" => false,
-                   'message' => "Credential Wrong!!!",
-               ], 401);
-           }
+            if ($request->password != $user->password) {
+                return response()->json([
+                    "success" => false,
+                    'message' => "Credential Wrong!!!",
+                ], 401);
+            }
             return response()->json([
-                "success"=>true,
-                'message'=>"Login Success!!!",
-            ],200);
-
-       }catch(\Exception $e){
+                "success" => true,
+                'message' => "Login Success!!!",
+                'role' => $user->role
+            ], 200);
+        } catch (\Exception $e) {
             return response()->json([
-              "success"=>false,
-              'message'=>"Something Went Wrong!!!".$e->getMessage(),
-            ],500);
-       }
+                "success" => false,
+                'message' => "Something Went Wrong!!!" . $e->getMessage(),
+            ], 500);
+        }
     }
 }
